0392c64fd905bfe235ae0d2a4da519facce3f5d0
hacodeml <hacodeml@gmail.com>
2019-03-21 14:49:17 -0500

    all pages except responsive

---
+++ assets/scripts/main.js
@@ -0,0 +1,18 @@
+function displayonoff1() {
+    var x = document.getElementById("social-media-menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayonoff2(e) {
+    e
+    var x = document.getElementById("search-window");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
